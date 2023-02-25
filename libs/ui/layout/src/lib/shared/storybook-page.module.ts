b6a42a5396e1619be108926d398eac0155ae0a9f
Mykola Babchenko <mykolababchenko@truckstop.com>
2023-02-25 22:32:01 +0100

    adds layout

--- libs/ui/layout/src/lib/shared/storybook-page.module.ts
+++ libs/ui/layout/src/lib/shared/storybook-page.module.ts
@@ -14,7 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    RouterModule.forRoot([]),
+    RouterModule,
   ],
   exports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
 })
