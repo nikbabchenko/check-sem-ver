23b55757c5b632355f2e9f6d05b25f855f7db9e3
Mykola Babchenko <mykolababchenko@truckstop.com>
2023-02-25 13:47:21 +0100

    adds ui pages

---
+++ libs/ui/layout/src/lib/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mmui-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent {}
--- libs/ui/layout/src/lib/ui-layout.module.ts
+++ libs/ui/layout/src/lib/ui-layout.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageLayoutComponent } from './page-layout/page-layout.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [HeaderComponent, FooterComponent, PageLayoutComponent],
 })
 export class UiLayoutModule {}
